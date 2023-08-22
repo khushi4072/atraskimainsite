105608243919c56f0af0cc6cce06d6a53e4343b6
khushi4072 <130855209+khushi4072@users.noreply.github.com>
2023-08-22 16:30:39 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ const cors=require('cors');
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
-app.use(express.static(path.join(__dirname, "../Frontend")))
+app.use(express.static(path.join(__dirname, "../Frontend/build")))
 
 
 
