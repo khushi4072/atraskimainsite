f6efa353e8ca82e2ad6adcafeec7599bdb863688
khushi4072 <130855209+khushi4072@users.noreply.github.com>
2023-08-22 13:46:08 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,7 +10,12 @@ const cors=require('cors');
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
-app.listen(5000)
+app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+
+
+
+app.listen(process.env.PORT || 5000);
 app.post('/formdata',async(req,resp)=>{
     let data= new form({
         name: req.body.name,
